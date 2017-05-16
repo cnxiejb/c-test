303bf1a6f229893c4d21606799da01194266dd85
谢金宝 <xiejinbao@corp.netease.com>
2017-05-16 16:51:25 +0800

    添加关于map的测试代码

---
+++ hello/stlTest/mapTest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "mapTest.hpp"
+
+xieMap::xieMap()
+{
+    std::cout<<"i am a xiemap"<<std::endl;
+}
+void xieMap::insertEntryTest()
+{
+    typedef std::map<std::string,float> Coll;
+    Coll coll;
+
+    coll.insert({"cott",22.3});
+    coll.insert(Coll::value_type("coott",32.3));
+    coll.insert(std::pair<std::string,float>("pair",12.3));
+    coll.insert(std::make_pair("make_pair",87.6));
+}
---
+++ hello/stlTest/mapTest.hpp
@@ -0,0 +1,12 @@
+#ifndef _H_MAPTEST_
+#define _H_MAPTEST_
+#include <map>
+
+class xieMap
+{
+    public:
+         xieMap();
+         void insertEntryTest();
+};
+
+#endif
